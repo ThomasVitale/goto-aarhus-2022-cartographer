5d078e6dadf6d8a1209b80d07784dc1347b49931
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-06-15 00:49:25 +0200

    Update application

--- applications/music-service/src/main/java/com/thomasvitale/musicservice/MusicServiceApplication.java
+++ applications/music-service/src/main/java/com/thomasvitale/musicservice/MusicServiceApplication.java
@@ -1,7 +1,13 @@
 package com.thomasvitale.musicservice;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class MusicServiceApplication {
